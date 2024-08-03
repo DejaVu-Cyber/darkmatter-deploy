70467ad8dc7fa799f2147fcb8e099acfbeacff68
Trent Moyar <trentmoyar@gmail.com>
2024-08-03 00:26:08 -0400

    Run pywb

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # darkmatter-deploy
 Scripts to deploy the final product.
+
+
+## Deploying pywb
+Init the webarchive by calling
+
+    ./initarchive.sh
+
+Then build the program by calling 
+
+    docker compose up
+
---
+++ initarchive.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+mkdir -p webarchive/collections/en
+mkdir webarchive/collections/en/archive
+mkdir webarchive/collections/en/indexes
+mkdir -p webarchive/collections/cn/archive
+mkdir webarchive/collections/cn/indexes
